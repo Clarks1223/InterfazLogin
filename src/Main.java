c22ce59058e22f928658baaecdefb090e0291dce
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-02-04 00:46:14 -0500

    Agrego conversor de unidades

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         Log login= new Log();
         //Abro el login
         login.abrirVentana();
+
+        //Pagina1 pag= new Pagina1("Administrador","Pablo Uchuari","23",
+          //      "pablo.uchuari@epn.edu.ec","3","5","03/02/2023");
+        //pag.abrirVentana();
     }
 }
--- src/Pagina1.java
+++ src/Pagina1.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
 
 public class Pagina1 extends Ventanas{
+    DecimalFormat dcf=new DecimalFormat("#.00");
     private JPanel Pag1;
     private JLabel LRol;
     private JLabel LMensaje;
@@ -9,7 +15,6 @@ public class Pagina1 extends Ventanas{
     private JLabel LCorreo;
     private JLabel LExperiencia;
     private JLabel LMensajes;
-    private JLabel LPerfil;
     private JLabel LIngreso;
     private JLabel LsetNombre;
     private JLabel LsetEdad;
@@ -18,6 +23,12 @@ public class Pagina1 extends Ventanas{
     private JLabel LsetNumMens;
     private JLabel LsetDateLog;
     private JLabel LLogo;
+    private JLabel LConversonDivisas;
+    private JTextField TFIngresarDolares;
+    private JButton BCalcularEuros;
+    private JLabel LResultadoEuros;
+    private JLabel LConversorDivisas;
+    private JLabel LPerfil;
 
     public Pagina1( String rol,String nom, String edad, String mail, String exp, String sms, String ultimoingreso){
         LRol.setText(rol);
@@ -27,12 +38,31 @@ public class Pagina1 extends Ventanas{
         LsetExp.setText(exp);
         LsetNumMens.setText(sms);
         LsetDateLog.setText(ultimoingreso);
+        TFIngresarDolares.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                TFIngresarDolares.setText("");
+            }
+        });
+        BCalcularEuros.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String datos="";
+                double origin=Double.parseDouble(TFIngresarDolares.getText()), tot=0;
+                tot=origin*0.92;
+                datos=dcf.format(tot)+"€";
+                LResultadoEuros.setText(datos);
+            }
+        });
     }
 
     @Override
     public JFrame abrirVentana(){
         JFrame frame= new JFrame("");
-        frame.setContentPane(new Pagina1(LRol.getText(), "ING."+LsetNombre.getText(),LsetEdad.getText()+" años", LsetMail.getText(),LsetExp.getText()+" años",LsetNumMens.getText()+" nuevos",LsetDateLog.getText()).Pag1);
+        frame.setContentPane(new Pagina1(LRol.getText(), "ING."+LsetNombre.getText(),
+                LsetEdad.getText()+" años", LsetMail.getText(),LsetExp.getText()+" años",
+                LsetNumMens.getText()+" nuevos",LsetDateLog.getText()).Pag1);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -44,7 +74,6 @@ public class Pagina1 extends Ventanas{
     }
     @Override
     public void cerrarVentana(){
-
     }
 
 }
